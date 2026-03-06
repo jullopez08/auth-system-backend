a64d294a304d7de0fd9001ae0b773aa251572995
jullopez08 <jperdomo@iegabo.edu.co>
2026-03-05 22:31:14 -0500

    feat: finish user registration implementation

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterDto } from '../dto/register.dto';
+import { register } from 'module';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
+export class AuthController {
+
+    constructor(private readonly authService: AuthService){}
 @Post('register')
 register(@Body() RegisterDto: RegisterDto) {
     return this.authService.register(RegisterDto);
-}
-export class AuthController {}
+}}
